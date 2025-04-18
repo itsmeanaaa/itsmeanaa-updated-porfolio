f42fbe0874e148c04b3088593412929b7517efdb
Ana <74693547+itsmeanaaa@users.noreply.github.com>
2025-04-18 17:47:33 +0800

    Add files via upload
    Still Inprogress Porfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+  document.querySelector(".btn").addEventListener("click", () => {
+    alert("Navigating to portfolio...");
+    // Here, you can link to the portfolio section or another page
+  });
+</script>
